7053de5a04cabab80bf7e9aae71b40cdd024666c
Pardeep Dogra <pardeep.dogra@tesco.com>
2020-02-09 20:53:36 +0530

    create dataframe from an RDD

--- src/main/scala/com/learning/rdd/RddExaminer.scala
+++ src/main/scala/com/learning/rdd/RddExaminer.scala
@@ -1,6 +1,8 @@
 package com.learning.rdd
 
-import com.learning.spark.SparkInstance.sparkContext
+import com.learning.spark.SparkInstance.{session, sparkContext}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 object RddExaminer extends App {
 
@@ -13,13 +15,29 @@ object RddExaminer extends App {
   val rddFromCsv = createRddFromCsv
   rddFromCsv.collect().foreach(println)
 
+  val empDF = createDataFrameFromAnRdd
+  empDF.printSchema()
+  empDF.show()
+
+  private def createDataFrameFromAnRdd = {
+    val schema = new StructType()
+      .add(StructField("id", StringType, true))
+      .add(StructField("name", StringType, true))
+      .add(StructField("address", StringType, true))
+      .add(StructField("contact", StringType, true))
+      .add(StructField("empid", IntegerType, true))
+      .add(StructField("deptid", IntegerType, true))
+
+    session.createDataFrame(rddFromCollection, schema)
+  }
+
   private def createRddFromCsv = {
     val path = "src/main/resources/airlines/airlines.csv"
     sparkContext.textFile(path)
   }
 
   private def createRddFromCollection = {
-    val data = Array("1", "Name", "Address", "000-000-0000", 5, 1)
+    val data = Array(Row("1", "Name", "Address", "000-000-0000", 5, 1))
     sparkContext.parallelize(data)
   }
 }
