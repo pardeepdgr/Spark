153b56b1bf703e67b3dd47966c6fefc725376f84
Pardeep Dogra <pardeep.dogra@tesco.com>
2020-02-09 12:12:49 +0530

    spark-scala seed

---
+++ src/main/scala/com/learning/counter/WordCounter.scala
@@ -0,0 +1,18 @@
+package com.learning.counter
+
+import com.learning.spark.SparkInstance.session
+
+object WordCounter extends App {
+  val SPACE = " "
+  val lines = session.sparkContext.parallelize(
+    Seq("By the pricking of my thumbs, Something wicked this way comes. Open, locks, Whoever knocks!",
+      "Fair is foul, and foul is fair: Hover through the fog and filthy air.",
+      "Brevity is the soul of wit."))
+
+  val counts = lines
+    .flatMap(line => line.split(SPACE))
+    .map(word => (word, 1))
+    .reduceByKey(_ + _)
+
+  counts.foreach(println)
+}
---
+++ src/main/scala/com/learning/spark/SparkInstance.scala
@@ -0,0 +1,14 @@
+package com.learning.spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+package object SparkInstance {
+
+  implicit lazy val session: SparkSession = SparkSession.builder()
+    .master("local[*]")
+    .config("spark.sql.autoBroadcastJoinThreshold", "100000")
+    .getOrCreate()
+  implicit lazy val sparkContext: SparkContext = session.sparkContext
+  implicit lazy val sqlContext: SQLContext = session.sqlContext
+}
