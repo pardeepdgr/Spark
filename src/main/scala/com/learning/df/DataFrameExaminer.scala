4629ca3ba8c7c562a03e06c47cb02ab42b24ae44
Pardeep Dogra <pardeep.dogra@tesco.com>
2020-02-09 22:53:18 +0530

    create dataframe from a csv

--- src/main/scala/com/learning/df/DataFrameExaminer.scala
+++ src/main/scala/com/learning/df/DataFrameExaminer.scala
@@ -6,4 +6,10 @@ object DataFrameExaminer extends App {
   val dfFromRange = session.range(5)
   dfFromRange.show()
 
+  val dfFromCsv = session.read
+    .format("csv")
+    .option("header", true)
+    .option("inferSchema", true)
+    .load("src/main/resources/airlines/flights.csv")
+  dfFromCsv.printSchema()
 }
