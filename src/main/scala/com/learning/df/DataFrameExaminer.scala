453444c8ae30b0c69fa89e33bf807073b94dd826
Pardeep Dogra <pardeep.dogra@tesco.com>
2020-02-10 10:13:26 +0530

    add more operations for DF

--- src/main/scala/com/learning/df/DataFrameExaminer.scala
+++ src/main/scala/com/learning/df/DataFrameExaminer.scala
@@ -3,20 +3,38 @@ package com.learning.df
 import com.learning.spark.SparkInstance.session
 
 object DataFrameExaminer extends App {
-  val dfFromRange = session.range(5)
-  dfFromRange.show()
+  val rangeDf = session.range(5)
 
-  val dfFromCsv = session.read
+  val flightDf = session.read
     .format("csv")
     .option("header", true)
     .option("inferSchema", true)
     .load("src/main/resources/airlines/flights.csv")
-  dfFromCsv.printSchema()
+  flightDf.printSchema()
+  flightDf.describe()
+  flightDf.limit(5).show()
+  // OR //
+  flightDf.show(5)
+  flightDf.show(false)
 
-  dfFromCsv.select("origin", "destination").show()
+  flightDf.select("origin", "destination")
+  flightDf.drop("air_time", "arrival_delay", "departure_delay")
+  flightDf.distinct()
+
   import session.implicits._
-  val totalTimeDF = dfFromCsv
-    .select($"air_time" - $"departure_delay" + $"arrival_delay")
+  flightDf.select($"air_time" - $"departure_delay" + $"arrival_delay")
     .withColumnRenamed("((air_time - departure_delay) + arrival_delay)", "total_time")
-    .show()
+
+  flightDf.filter("destination = 'LAX'")
+  // OR //
+  flightDf.filter(flightDf("destination")==="LAX")
+  flightDf.filter(flightDf("destination").isin("LAX","JFK"))
+
+  flightDf.groupBy("flight_number").count()
+  flightDf.groupBy("flight_number").sum("air_time")
+
+  import org.apache.spark.sql.functions._
+  flightDf.orderBy(desc("air_time"))
+
+  flightDf.stat.crosstab("origin","distance")
 }
