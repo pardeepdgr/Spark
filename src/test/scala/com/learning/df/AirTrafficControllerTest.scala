ca0c0f1fbb12141ea146ae27a8fa2b923c9645d3
Pardeep Dogra <pardeepdgr@gmail.com>
2020-04-15 10:26:13 +0530

    create tests for basic dataframe operations

---
+++ src/test/scala/com/learning/df/AirTrafficControllerTest.scala
@@ -0,0 +1,54 @@
+package com.learning.df
+
+import base.TestBootstrap
+import base.TestHelper.readCSV
+import base.TestSetup.{init, kill, session}
+import org.apache.spark.sql.DataFrame
+
+class AirTrafficControllerTest extends TestBootstrap {
+  private val FLIGHTS = "src/test/resources/airlines/flights.csv"
+  private val AIRLINES = "src/test/resources/airlines/airlines.csv"
+
+  private var flights: DataFrame = _
+  private var airlines: DataFrame = _
+
+  before {
+    init("AirTrafficControllerTest", "local")
+    flights = readCSV(session, FLIGHTS)
+    airlines = readCSV(session, AIRLINES)
+  }
+
+  it should "print schema of flights in tree format" in {
+    flights.printSchema()
+  }
+
+  it should "describe vital stats(count, max, min, mean, stddev) of flights" in {
+    flights.describe().show()
+  }
+
+  it should "print 5 element on console without any truncation of long column-values" in {
+    airlines.limit(5).show(false)
+  }
+
+  it should "fetch all columns name of flights" in {
+    val columns = airlines.describe().columns
+    assert(columns.sameElements(Array("summary", "Code", "Description")), "column mismatch")
+  }
+
+  it should "select all origins and destinations of flights" in {
+    flights.select("origin", "destination").show()
+  }
+
+  it should "delete delays of flights" in {
+    flights.drop("arrival_delay", "departure_delay").show()
+  }
+
+  it should "select non-duplicate origin of all flights" in {
+    flights.select("origin").distinct().show()
+  }
+
+  after {
+    kill
+  }
+
+}
