37e9b1b509e1ca3a1766394f691c4fa7dce8bccc
Pardeep Dogra <pardeepdgr@gmail.com>
2020-04-16 15:46:58 +0530

    extract and join data from DFs

--- src/main/scala/com/learning/df/SoccerTalentScout.scala
+++ src/main/scala/com/learning/df/SoccerTalentScout.scala
@@ -1,70 +1,16 @@
 package com.learning.df
 
-import com.learning.helper.DataFrameAssistant
-import com.learning.helper.DataFrameCreator
-import com.learning.spark.SparkInstance.{session, sparkContext}
-import org.apache.spark.sql.{DataFrame, Row}
-import org.apache.spark.util.LongAccumulator
+import org.apache.spark.sql.DataFrame
 
 object SoccerTalentScout {
-  val PLAYER_ATTRIBUTE_DATASET_PATH = "src/main/resources/soccer/player_attributes.csv"
-  val PLAYER_DATASET_PATH = "src/main/resources/soccer/player.csv"
 
-  def main(args: Array[String]): Unit = {
-    val players = DataFrameCreator.fromCsv(session, PLAYER_DATASET_PATH)
-    val playerAttributes = DataFrameCreator.fromCsv(session, PLAYER_ATTRIBUTE_DATASET_PATH)
-
-    val vitalStats: DataFrame = getAverageOfVitalStats(playerAttributes)
-    val strikers: DataFrame = getWeightedStats(vitalStats)
-    val topStrikers: DataFrame = filterTopStrikersInDescOrder(strikers)
-
-    DataFrameAssistant.showPhysicalAndLogicalPlan(topStrikers)
-
-    val topStrikerPlayersWithDuplicateId: DataFrame = DataFrameAssistant.equiJoin(players, topStrikers, "player_api_id")
-    val topStrikerPlayersWithoutDuplicateId: DataFrame = DataFrameAssistant.naturalJoin(players, topStrikers, "player_api_id")
-
-    topStrikerPlayersBroadcastJoin(players, topStrikers)
-    playerHeadingAccuracyUsingAccumulator(topStrikerPlayersWithoutDuplicateId)
-
+  def getAverageOfVitalStats(playerAttributes: DataFrame): DataFrame = {
+    playerAttributes
+      .groupBy("player_api_id")
+      .avg("finishing", "shot_power", "acceleration")
   }
 
-  private def playerHeadingAccuracyUsingAccumulator(topStrikers: DataFrame) = {
-    //Use AccumulatorV2 to create user defined accumulators
-    val shortCount = sparkContext.longAccumulator("Counter for short players")
-    val mediumCount = sparkContext.longAccumulator("Counter for average height players")
-    val tallCount = sparkContext.longAccumulator("Counter for tall players")
-
-    topStrikers.foreach(record => countPlayersHeight(record, shortCount, mediumCount, tallCount))
-  }
-
-  private def countPlayersHeight(record: Row,
-                                 shortCount: LongAccumulator,
-                                 mediumCount: LongAccumulator,
-                                 tallCount: LongAccumulator): Unit = {
-    val height: Double = record.getAs[Double]("height")
-    if (height <= 175)
-      shortCount.add(1)
-    else if (height > 175 & height < 195)
-      mediumCount.add(1)
-    else if (height > 195)
-      tallCount.add(1)
-  }
-
-  private def topStrikerPlayersBroadcastJoin(players: DataFrame, topStrikers: DataFrame): DataFrame = {
-    import org.apache.spark.sql.functions.broadcast
-    players
-      .select("player_api_id", "player_name") // NOTE: while join, first select what all column required then do join
-      .join(broadcast(topStrikers), "player_api_id")
-  }
-
-  private def filterTopStrikersInDescOrder(strikers: DataFrame) = {
-    import org.apache.spark.sql.functions.desc
-    strikers
-      .filter(strikers("striker_grade") > 100)
-      .orderBy(desc("striker_grade"))
-  }
-
-  private def getWeightedStats(vitalStats: DataFrame) = {
+  def getWeightedStats(vitalStats: DataFrame): DataFrame = {
     import org.apache.spark.sql.functions.round
     vitalStats
       .withColumn("striker_grade",
@@ -74,9 +20,18 @@ object SoccerTalentScout {
       .drop("avg(finishing)", "avg(shot_power)", "avg(acceleration)")
   }
 
-  private def getAverageOfVitalStats(playerAttributes: DataFrame) = {
-    playerAttributes
-      .groupBy("player_api_id")
-      .avg("finishing", "shot_power", "acceleration")
+  def getTopStrikers(strikers: DataFrame): DataFrame = {
+    import org.apache.spark.sql.functions.desc
+    strikers
+      .filter(strikers("striker_grade") > 100)
+      .orderBy(desc("striker_grade"))
   }
+
+  def getTopStrikersDetail(players: DataFrame, topStrikers: DataFrame): DataFrame = {
+    import org.apache.spark.sql.functions.broadcast
+    players
+      .select("player_api_id", "player_name") // NOTE: for performance, first select what all column required then do join
+      .join(broadcast(topStrikers), "player_api_id")
+  }
+
 }
---
+++ src/test/scala/com/learning/df/SoccerTalentScoutTest.scala
@@ -0,0 +1,62 @@
+package com.learning.df
+
+import base.TestBootstrap
+import base.TestHelper.readCSV
+import base.TestSetup.{init, kill, session}
+import com.learning.df.SoccerTalentScout._
+import com.learning.helper.DataFrameAssistant.naturalJoin
+import org.apache.spark.sql.DataFrame
+
+class SoccerTalentScoutTest extends TestBootstrap {
+
+  private val PLAYER = "src/test/resources/soccer/player.csv"
+  private val PLAYER_ATTRIBUTE = "src/test/resources/soccer/player_attributes.csv"
+
+  private var players: DataFrame = _
+  private var playerAttributes: DataFrame = _
+
+  before {
+    init("SoccerTalentScoutTest", "local")
+    players = readCSV(session, PLAYER)
+    playerAttributes = readCSV(session, PLAYER_ATTRIBUTE)
+  }
+
+  it should "calculate average of all vital stats of all players" in {
+    val vitalStats: DataFrame = getAverageOfVitalStats(playerAttributes)
+    vitalStats.show()
+  }
+
+  it should "calculate striker grade of all players" in {
+    val vitalStats: DataFrame = getAverageOfVitalStats(playerAttributes)
+    val strikers: DataFrame = getWeightedStats(vitalStats)
+    strikers.show()
+  }
+
+  it should "find top strikers" in {
+    val vitalStats: DataFrame = getAverageOfVitalStats(playerAttributes)
+    val strikers: DataFrame = getWeightedStats(vitalStats)
+    val topStrikers: DataFrame = getTopStrikers(strikers)
+    topStrikers.show()
+  }
+
+  it should "get top strikers detail" in {
+    val vitalStats: DataFrame = getAverageOfVitalStats(playerAttributes)
+    val strikers: DataFrame = getWeightedStats(vitalStats)
+    val topStrikers: DataFrame = getTopStrikers(strikers)
+    val topStrikersDetail: DataFrame = naturalJoin(players, topStrikers, "player_api_id")
+    topStrikersDetail.show()
+  }
+
+  it should "get top strikers detail with performance" in {
+    val vitalStats: DataFrame = getAverageOfVitalStats(playerAttributes)
+    val strikers: DataFrame = getWeightedStats(vitalStats)
+    val topStrikers: DataFrame = getTopStrikers(strikers)
+    val topStrikersDetail: DataFrame = getTopStrikersDetail(players, topStrikers)
+    topStrikersDetail.show()
+  }
+
+  after {
+    kill
+  }
+
+}
