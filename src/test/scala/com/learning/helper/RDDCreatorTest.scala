f9ef9c5a42b2048370798bee4de40778e241ceb8
Pardeep Dogra <pardeepdgr@gmail.com>
2020-05-28 13:58:49 +0530

    add tests for RDD creation

---
+++ src/test/scala/com/learning/helper/RDDCreatorTest.scala
@@ -0,0 +1,36 @@
+package com.learning.helper
+
+import base.TestBootstrap
+import base.TestSetup.{init, kill, session}
+import com.learning.helper.RDDCreator.{fromCsv, fromRows}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Row
+
+class RDDCreatorTest extends TestBootstrap {
+
+  private val AIRLINES = "src/test/resources/airlines/airlines.csv"
+
+  before {
+    init("RDDCreatorTest", "local")
+  }
+
+  behavior of "RDD Creation"
+
+  it should "create a RDD from given Rows" in {
+    val data = Array(Row("1", "Name", "Address", "000-000-0000", 5, 1))
+    val rdd: RDD[Row] = fromRows(session.sparkContext, data)
+
+    assert(rdd.count() == 1, "RDD is empty.")
+  }
+
+  it should "create a RDD from given CSV file" in {
+    val rdd: RDD[String] = fromCsv(session.sparkContext, AIRLINES)
+
+    assert(rdd.count() == 1580)
+  }
+
+  after {
+    kill
+  }
+
+}
--- src/test/scala/com/learning/rdd/RDDAnalyzerTest.scala
+++ src/test/scala/com/learning/rdd/RDDAnalyzerTest.scala
@@ -19,19 +19,6 @@ class RDDAnalyzerTest extends TestBootstrap {
     airlines = fromCsv(session.sparkContext, AIRLINES)
   }
 
-  it should "create a RDD from given Rows" in {
-    val data = Array(Row("1", "Name", "Address", "000-000-0000", 5, 1))
-    val rdd: RDD[Row] = fromRows(session.sparkContext, data)
-
-    assert(rdd.count() == 1, "RDD is empty.")
-  }
-
-  it should "create a RDD from given CSV file" in {
-    val rdd: RDD[String] = fromCsv(session.sparkContext, AIRLINES)
-
-    assert(rdd.count() == 1580, "Partial RDD is created.")
-  }
-
   it should "print all data of an RDD" in {
     airlines.collect().foreach(println)
   }
