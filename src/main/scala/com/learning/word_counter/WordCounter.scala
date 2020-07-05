ac554a790f541a2659b2064adc226a897b1e5578
Pardeep Dogra <pardeepdgr@gmail.com>
2020-07-05 17:21:28 +0530

    count words and partition the result by 3 different partitioners

--- src/main/scala/com/learning/word_counter/WordCounter.scala
+++ src/main/scala/com/learning/word_counter/WordCounter.scala
@@ -1,18 +1,49 @@
 package com.learning.word_counter
 
-import com.learning.SparkInstance.session
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{HashPartitioner, Partitioner, RangePartitioner}
 
-object WordCounter extends App {
-  val SPACE = " "
-  val lines = session.sparkContext.parallelize(
-    Seq("By the pricking of my thumbs, Something wicked this way comes. Open, locks, Whoever knocks!",
-      "Fair is foul, and foul is fair: Hover through the fog and filthy air.",
-      "Brevity is the soul of wit."))
+object WordCounter {
+  private val SPACE = " "
+  private val NUMBER_OF_PARTITIONS = 3
 
-  val counts = lines
-    .flatMap(line => line.split(SPACE))
-    .map(word => (word, 1))
-    .reduceByKey(_ + _)
+  def usingDefaultHashPartitioning(text: RDD[String]): RDD[(String, Int)] = {
+    text
+      .flatMap(line => line.split(SPACE))
+      .map(word => (word, 1))
+      .partitionBy(new HashPartitioner(NUMBER_OF_PARTITIONS))
+      .reduceByKey(_ + _)
+  }
 
-  counts.foreach(println)
+  def usingRangePartitioning(text: RDD[String]): RDD[(String, Int)] = {
+    val words: RDD[(String, Int)] = text
+      .flatMap(line => line.split(SPACE))
+      .map(word => (word, 1))
+
+    words
+      .partitionBy(new RangePartitioner(NUMBER_OF_PARTITIONS, words))
+      .reduceByKey(_ + _)
+  }
+
+  def usingCustomPartitioning(text: RDD[String]): RDD[(String, Int)] = {
+    text
+      .flatMap(line => line.split(SPACE))
+      .map(word => (word, 1))
+      .partitionBy(new CustomPartitioner(NUMBER_OF_PARTITIONS))
+      .reduceByKey(_ + _)
+  }
+
+}
+
+class CustomPartitioner(numberOfPartitions: Int) extends Partitioner {
+  override def numPartitions: Int = numberOfPartitions
+
+  /** return partition ID {from 0 to numPartitions-1} */
+  override def getPartition(key: Any): Int = if (key.toString.equals("custom")) 0 else 1
+
+  /** check whether two of your RDDs are Partitioned in the same way or not */
+  override def equals(partition: Any): Boolean = partition match {
+    case other: CustomPartitioner => other.numPartitions == numPartitions
+    case _ => false
+  }
 }
---
+++ src/test/scala/com/learning/word_counter/WordCounterTest.scala
@@ -0,0 +1,43 @@
+package com.learning.word_counter
+
+import base.TestBootstrap
+import base.TestSetup.{init, kill, session}
+import org.apache.spark.rdd.RDD
+
+
+class WordCounterTest extends TestBootstrap {
+
+  private val data = Seq("By the pricking of my thumbs, Something wicked this way comes. Open, locks, Whoever knocks!",
+    "Fair is foul, and foul is fair: Hover through the fog and filthy air.",
+    "Brevity is the soul of wit.")
+
+  before {
+    init("WordCounterTest", "local")
+  }
+
+  behavior of "RDD partitioning"
+
+  it should "get word count in a partitioned rdd using default partitioning" in {
+    val text: RDD[String] = session.sparkContext.parallelize(data)
+
+    WordCounter.usingDefaultHashPartitioning(text).foreach(println)
+  }
+
+  it should "get word count in a partitioned rdd using range partitioning" in {
+    val text: RDD[String] = session.sparkContext.parallelize(data)
+
+    // ranges are determined by sampling the content of the RDD
+    WordCounter.usingRangePartitioning(text).foreach(println)
+  }
+
+  it should "get word count in a partitioned rdd using custom partitioning" in {
+    val text: RDD[String] = session.sparkContext.parallelize(data)
+
+    WordCounter.usingCustomPartitioning(text).foreach(println)
+  }
+
+  after {
+    kill
+  }
+
+}
